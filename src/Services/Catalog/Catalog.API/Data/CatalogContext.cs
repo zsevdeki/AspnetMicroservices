c53e92ad760d0eda55752576e85b588a3487800a
zskir <zsevdek@gmail.com>
2022-10-31 16:24:15 +0300

    Db Preperation

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext:ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client= new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DataseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(("DataseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string ImageFile { get; set; }
+        public string Price
+            { get; set; }   
+    }
+}
